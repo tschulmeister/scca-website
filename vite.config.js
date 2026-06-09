354a0b8614001e5fc26e68413451600926b6c46b
Tyler Schulmeister <tcschulmeister@gmail.com>
2026-06-08 20:58:17 -0400

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html', // Allows SPA-style routing for un-prerendered pages
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			// Only use the repo base path in production, keep root for local development
-			base: process.env.NODE_ENV === 'production' ? '/scca-website' : ''
-		}
-	}
-};
-
-export default config;
+export default defineConfig({
+  plugins: [sveltekit()], 
+});
