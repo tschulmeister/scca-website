261a6186cf5e52936c138680501e1f2c0a69dbcf
Tyler Schulmeister <tcschulmeister@gmail.com>
2026-06-08 20:55:42 -0400

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
-  plugins: [sveltekit()]
-});
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html', // Allows SPA-style routing for un-prerendered pages
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			// Only use the repo base path in production, keep root for local development
+			base: process.env.NODE_ENV === 'production' ? '/scca-website' : ''
+		}
+	}
+};
+
+export default config;
