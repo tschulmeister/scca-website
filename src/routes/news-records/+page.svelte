dcc1c8e53e839b2b6ddc20f5e150357baabb0b30
Tyler Schulmeister <tcschulmeister@gmail.com>
2026-07-01 20:50:53 -0400

    add static newsletter file

--- src/routes/news-records/+page.svelte
+++ src/routes/news-records/+page.svelte
@@ -18,7 +18,7 @@
       class="mt-8 flex flex-col items-center gap-4 sm:flex-row sm:justify-center"
     >
       <a
-        href="https://www.shipleyschoice.org/uploads/1/4/6/6/146632212/newsletter_2025.pdf"
+        href="/data/newsletter/2025-10.pdf"
         target="_blank"
         rel="noreferrer"
         class="inline-flex items-center justify-center rounded-2xl bg-blue-600 px-6 py-3 text-white font-semibold shadow-sm hover:bg-blue-700 transition-colors"
