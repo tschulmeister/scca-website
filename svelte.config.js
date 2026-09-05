2b6e0fd435dc2eb22d54f2e18e1cb22797a57267
tcschul <tcschul@example.com>
2026-09-05 13:47:40 -0400

    added news item about brevo signups

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-auto will seamlessly optimize the build specifically for Vercel
-		adapter: adapter(),
-		alias: {
-			$data: 'src/data',
-			$components: 'src/components'
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    // adapter-auto will seamlessly optimize the build specifically for Vercel
+    adapter: adapter(),
+    alias: {
+      $data: "src/data",
+      $components: "src/components",
+    },
+  },
 };
 
 export default config;
